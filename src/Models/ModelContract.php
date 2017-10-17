d76e732358c22983e780a1f9f32346758d1455ab
Dylan de Wit <ddewit@swis.nl>
2017-10-17 14:45:14 +0200

    Updated model.stub

--- src/Models/ModelContract.php
+++ src/Models/ModelContract.php
@@ -4,10 +4,6 @@ namespace Swis\LaravelApi\Models;
 
 interface ModelContract
 {
-    public function getRelationships(): array;
-
-    public function getRepository(): string;
-
     public function getTranslatable();
 
     public function getRules($id = null): array;
