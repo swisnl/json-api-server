9d3e174b7889c23fe0f949a19b39bd3fb5c52ee0
bjorn <bjorn@swis.nl>
2018-10-30 10:21:15 +0100

    Add `is_array` to the countable check should a plain array be the result.

--- src/Http/Resources/BaseApiResource.php
+++ src/Http/Resources/BaseApiResource.php
@@ -165,7 +165,7 @@ class BaseApiResource extends Resource
             }
 
             $data = $this->resource->$relationship;
-            if ($data instanceof Countable && 0 == count($data)) {
+            if ((is_array($data) || $data instanceof \Countable) && 0 == count($data)) {
                 continue;
             }
 
